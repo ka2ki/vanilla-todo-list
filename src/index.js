782dc6f8f99359ab1eb376333e038d9764b6866f
katsuki.iwamoto <43980437+ka2ki@users.noreply.github.com>
2021-12-27 15:10:24 +0900

    [update] 新規追加で未完了のTODOへ項目追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./styles.scss";
+
+const onClickAdd = () => {
+  // input[text]の値を取得し、初期化する
+  const addText = document.getElementById("add_text");
+  const addTextValue = addText.value;
+  addText.value = "";
+
+  // liタグの生成
+  const li = document.createElement("li");
+  li.innerHTML = addTextValue;
+
+  // 未完了のTODOエリアへタグ設置
+  const unfinishedList = document.getElementById("unfinished_list");
+  unfinishedList.appendChild(li);
+};
+
+// 新規追加のクリックイベント
+document
+  .getElementById("add_btn")
+  .addEventListener("click", () => onClickAdd());
